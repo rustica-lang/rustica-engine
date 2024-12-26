34ceeaf16c6bdd752a42724942a9f704a51187a7
Xenia Lu <xenia.lyy@gmail.com>
2024-12-26 09:31:17 +0800

    Clean up properly in rst_load_module()

--- src/rustica/module.c
+++ src/rustica/module.c
@@ -47,29 +47,37 @@ rst_module_worker_teardown() {
 void
 rst_load_module(const char *name, uint8 **buffer, uint32 *size) {
     Datum name_datum = CStringGetTextDatum(name);
-    debug_query_string = load_module_sql;
-    int ret = SPI_execute_plan(load_module_plan, &name_datum, NULL, true, 1);
 
-    if (ret != SPI_OK_SELECT)
-        ereport(ERROR,
-                errmsg("failed to load module \"%s\": %s",
-                       name,
-                       SPI_result_code_string(ret)));
+    PG_TRY();
+    {
+        debug_query_string = load_module_sql;
+        int ret =
+            SPI_execute_plan(load_module_plan, &name_datum, NULL, true, 1);
+        if (ret != SPI_OK_SELECT)
+            ereport(ERROR,
+                    errmsg("failed to load module \"%s\": %s",
+                           name,
+                           SPI_result_code_string(ret)));
+        if (SPI_processed == 0)
+            ereport(ERROR,
+                    errcode(ERRCODE_NO_DATA_FOUND),
+                    errmsg("module \"%s\" doesn't exist", name));
 
-    if (SPI_processed == 0)
-        ereport(ERROR,
-                errcode(ERRCODE_NO_DATA_FOUND),
-                errmsg("module \"%s\" doesn't exist", name));
+        bool isnull;
+        bytea *bin_code = DatumGetByteaPP(SPI_getbinval(SPI_tuptable->vals[0],
+                                                        SPI_tuptable->tupdesc,
+                                                        1,
+                                                        &isnull));
+        Assert(!isnull);
 
-    bool isnull;
-    bytea *bin_code = DatumGetByteaPP(SPI_getbinval(SPI_tuptable->vals[0],
-                                                    SPI_tuptable->tupdesc,
-                                                    1,
-                                                    &isnull));
-    debug_query_string = NULL;
-    Assert(!isnull);
-
-    *buffer = (uint8 *)VARDATA_ANY(bin_code);
-    *size = VARSIZE_ANY_EXHDR(bin_code);
-    Assert(*buffer != NULL);
+        *buffer = (uint8 *)VARDATA_ANY(bin_code);
+        *size = VARSIZE_ANY_EXHDR(bin_code);
+        Assert(*buffer != NULL);
+    }
+    PG_FINALLY();
+    {
+        pfree(DatumGetPointer(name_datum));
+        debug_query_string = NULL;
+    }
+    PG_END_TRY();
 }
